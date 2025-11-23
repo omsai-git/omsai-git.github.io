091074e6393a048fd6767cb46c6f513c806abf1d
omsai-git <omsai.kusuma@gmail.com>
2025-11-24 02:19:20 +0530

    persisting count in local storage

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,16 @@
-import { useState } from 'react'
+import { useCallback, useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(Number(localStorage.getItem('count') || 0))
+
+  const updateCount = useCallback(() => {
+    let updatedCount = count + 1;
+    localStorage.setItem('count', String(updatedCount))
+    setCount(updatedCount)
+  }, [count])
 
   return (
     <>
@@ -18,7 +24,7 @@ function App() {
       </div>
       <h1>Vite + React</h1>
       <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
+        <button onClick={updateCount}>
           count is {count}
         </button>
         <p>
